ad6395bfacbd2b5e61182e8601970d16e2a4b6f2
MitoYuuki <nekoneko047@gmail.com>
2026-03-21 18:38:13 +0900

    演習問題5章

---
+++ chapter5_1/Main.java
@@ -0,0 +1,20 @@
+package chapter5_1;
+
+public class Main {
+  public static void main(String[] args) {
+    introduceOneself();
+  }
+
+  public static void introduceOneself(){
+    String name = "Mito";
+    int age = 33;
+    double height =167.9;
+    String zodiac ="獅子";
+    System.out.println("私の名前は"+name+"です");
+    System.out.println("歳は"+age+"です");
+    System.out.println("身長は"+height+"cmです");
+    System.out.println("十二支は"+zodiac+"です");
+  }
+}
+
+
